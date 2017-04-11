ae41369f6650e9850b08756e13afab8f353c219f
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-04-12 01:57:56 +0430

    Debug fix

--- install.sh
+++ install.sh
@@ -38,8 +38,7 @@ sudo apt-get update
 sudo apt-get upgrade
 
 sudo apt-get dist-upgrade
-wget https://valtman.name/files/telegram-cli-1222
-mv telegram-cli-1222 tg
+
 wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
  tar zxpf luarocks-2.2.2.tar.gz
  cd luarocks-2.2.2
@@ -55,6 +54,8 @@ wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
  sudo service redis-server start
  cd ..
  rm -rf luarocks-2.2.2.tar.gz
+ wget https://valtman.name/files/telegram-cli-1222
+ mv telegram-cli-1222 tg
  chmod +x tg
  chmod +x anticrash.sh
  sudo apt-get install python-setuptools python-dev build-essential
