a44ec3b69163293a119531057e7c7af4e74180b4
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-04-21 06:15:35 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,29 @@
-
-#!/bin/bash
+#!/usr/bin/env bash
 
 THIS_DIR=$(cd $(dirname $0); pwd)
 cd $THIS_DIR
 
 update() {
   git pull
-  git submodule update --init --recursive
+  install_luarocks
   install_rocks
 }
 #By: @sajjad_021
 # Will install luarocks on THIS_DIR/.luarocks
 install_luarocks() {
+  sudo apt-get install luarocks
   git clone https://github.com/keplerproject/luarocks.git
   cd luarocks
+  ./configure; sudo make bootstrap
+ sudo luarocks install luasocket
+ sudo luarocks install luasec
+ sudo luarocks install redis-lua
+ sudo luarocks install lua-term
+ sudo luarocks install serpent
+ sudo luarocks install dkjson
+ sudo luarocks install lanes
+ sudo luarocks install Lua-cUR  
+ 
   git checkout tags/v2.4.2 # Current stable
 
   PREFIX="$THIS_DIR/.luarocks"
@@ -39,7 +49,17 @@ install_rocks() {
     then echo "Error. Exiting."; exit $RET;
   fi
 
+  ./.luarocks/bin/luarocks install xml
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
 
+  ./.luarocks/bin/luarocks install feedparser
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+  
+  
  ./.luarocks/bin/luarocks install Lua-cURL
   RET=$?; if [ $RET -ne 0 ];
     then echo "Error. Exiting."; exit $RET;
@@ -81,16 +101,6 @@ install_rocks() {
   fi
 
   ./.luarocks/bin/luarocks install fakeredis
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install xml
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install feedparser
   RET=$?; if [ $RET -ne 0 ];
     then echo "Error. Exiting."; exit $RET;
   fi
