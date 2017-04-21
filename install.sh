6634e79833e271fa2837ac91ac87d7dda3fb7842
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-04-21 08:06:59 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,20 +4,19 @@ THIS_DIR=$(cd $(dirname $0); pwd)
 cd $THIS_DIR
 
 update() {
-  git pull
-  install_luarocks
-  install_rocks
+	sudo apt-get update
+	sudo apt-get upgrade
+  cd $home && killall tmux && killall bash && killall screen && killall tg && killall telegram-cli && killall telegram-cli-1222 && rm -rf sinchi && rm -rf SinChi && rm -rf .telegram-cli && sudo apt-get autoclean && sudo apt-get autoremove && git clone https://github.com/tgMember/sinchi.git && cd sinchi && chmod +x install.sh && ./install.sh -y
 }
+
 #By: @sajjad_021
 # Will install luarocks on THIS_DIR/.luarocks
 install_luarocks() {
-  sudo apt-get install python-setuptools python-dev build-essential
- sudo easy_install pip
- sudo pip install redis
-  sudo apt-get install luarocks
   git clone https://github.com/keplerproject/luarocks.git
   cd luarocks
-  ./configure; sudo make bootstrap
+   sudo apt-get install luarocks
+ ./configure; sudo make bootstrap
+   make build && make install
  sudo luarocks install luasocket
  sudo luarocks install luasec
  sudo luarocks install redis-lua
@@ -26,93 +25,49 @@ install_luarocks() {
  sudo luarocks install dkjson
  sudo luarocks install lanes
  sudo luarocks install Lua-cUR  
- sudo service redis-server start
-
-  git checkout tags/v2.4.2 # Current stable
+  sudo service redis-server start
+  git checkout tags/v2.2.2 # Current stable
 
   PREFIX="$THIS_DIR/.luarocks"
 
   ./configure --prefix=$PREFIX --sysconfdir=$PREFIX/luarocks --force-config
-
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  make build && make install
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting.";exit $RET;
-  fi
-
+   sudo apt-get install luarocks
+ ./configure; sudo make bootstrap
+   make build && make install 
+	  sudo service redis-server start
   cd ..
   rm -rf luarocks
 }
  
 install_rocks() {
   ./.luarocks/bin/luarocks install luasocket
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
+ 
   ./.luarocks/bin/luarocks install xml
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
+  
   ./.luarocks/bin/luarocks install feedparser
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
   
-  
- ./.luarocks/bin/luarocks install Lua-cURL
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
  ./.luarocks/bin/luarocks install lua-term
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
 
  ./.luarocks/bin/luarocks install dkjson
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
+ 
  ./.luarocks/bin/luarocks install lanes
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
+ 
  ./.luarocks/bin/luarocks install luasec
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-	
+  
   ./.luarocks/bin/luarocks install oauth
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
+ 
   ./.luarocks/bin/luarocks install redis-lua
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
+ 
   ./.luarocks/bin/luarocks install lua-cjson
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
+ 
   ./.luarocks/bin/luarocks install fakeredis
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
+ 
+  ./.luarocks/bin/luarocks install Lua-cURL
 
   ./.luarocks/bin/luarocks install serpent
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
+	
+	 sudo ./.luarocks/bin/service redis-server start
+
 }
 
 install2() {
@@ -134,77 +89,44 @@ logo[5]="     ##      #######     ##       ## ######## ##       ## ####### #####
         printf "\n\t"
     done
 printf "\n"
-  	sudo apt-get update
-	sudo apt-get upgrade
     sudo apt-get install software-properties-common
-	  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test 
+		sudo apt-get update
+	  sudo apt-get upgrade
+    sudo apt-get dist-upgrade
 		sudo apt-get install g++-4.7 -y c++-4.7 -y
 		sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
 		sudo apt-get install screen -y
 		sudo apt-get install tmux -y
 		sudo apt-get install libstdc++6 -y
 		sudo apt-get install lua-lgi -y
-		sudo apt-get install libnotify-dev -y
+		sudo apt-get install libnotify-dev -y	
+		sudo apt-get install tor -y
+                sudo apt-get install openvpn -y		
+                sudo apt-get install network-manager-openvpn
+               	sudo apt-get update
+	        sudo apt-get upgrade
 		install_luarocks
-                install_rocks
-		sudo service redis-server start
-		sudo apt-get install python-setuptools python-dev build-essential
-                sudo easy_install pip
-                sudo pip install redis
-	  wget http://valtman.name/files/telegram-cli-1222
-          mv telegram-cli-1222 telegram-cli
-          chmod +x telegram-cli
-          chmod +x anticrash.sh
-  exit 1
-}
-
-sudos() {
-  read -p "Do you want to update the project? [y/n] = "
-	if [ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ]; then
-        sudo apt-get update
-	    wait
-	    sudo apt-get upgrade
-	    wait
-	    sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev -y
-	    wait
-		install2
-    elif [ "$REPLY" == "n" ] || [ "$REPLY" == "N" ]; then
-        install2
-	else
-	    sudos
-    fi
+		sudo apt-get update
+	  sudo apt-get upgrade
+		install_rocks
 }
 
 install() {
-  read -p "Do you want to update the project? [y/n] = "
-	if [ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ]; then
-        sudo apt-get update
-	    wait
+      sudo apt-get update
 	    sudo apt-get upgrade
-	    wait
-	    sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev -y
-	    wait
-		install2
-    elif [ "$REPLY" == "n" ] || [ "$REPLY" == "N" ]; then
-        install2
-	else
-	    sudos
-    fi
-}
-
-que() {
-read -p "Do you want to install files? [y/n] = "
-	if [ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ]; then
-        install
-    elif [ "$REPLY" == "n" ] || [ "$REPLY" == "N" ]; then
-        exit 1
-	else
-	    que
-fi
-}
-
-error() {
-sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev tmux subversion
+			install2
+			sudo apt-get update
+	                sudo apt-get upgrade
+			sudo service redis-server start
+			wget http://valtman.name/files/telegram-cli-1222
+                       mv telegram-cli-1222 telegram-cli
+		    chmod 777 telegram-cli
+                   chmod 777 anticrash.sh
+		  sudo apt-get install python-setuptools python-dev build-essential
+                sudo easy_install pip
+                sudo pip install redis
+	  echo "Was successfully installed"
 }
 
 #By: @sajjad_021
@@ -212,8 +134,6 @@ if [ "$1" = "install" ]; then
   install
 elif [ "$1" = "update" ]; then
   update
-elif [ "$1" = "error" ]; then
-  error
 else
   if [ ! -f telegram-cli ]; then
     echo -e "\033[38;5;208mError! Tg not found, Please reply to this message:\033[0;00m"
@@ -222,22 +142,13 @@ else
         install
     elif [ "$REPLY" == "n" ] || [ "$REPLY" == "N" ]; then
         exit 1
-	else
-	    que
-    fi
   fi
-
-  if [ ! -f telegram-cli ]; then
-    echo -e "\033[38;5;208mError! Tg not found, Please reply to this message:\033[0;00m"
-    read -p "Do you want to install starter files? [y/n] = "
-	if [ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ]; then
-        install
-    elif [ "$REPLY" == "n" ] || [ "$REPLY" == "N" ]; then
-        exit 1
-	else
-	    que
-    fi
 fi
-
+sudo service tor start		
+sudo service openvpn start		
+                export http_proxy="socks5://127.0.0.1:9150"		
+                export https_proxy="socks5://127.0.0.1:9150"		
+                export ftp_proxy="socks5://127.0.0.1:9150"
+sudo service redis-server start
   python sinchi.py
 fi
