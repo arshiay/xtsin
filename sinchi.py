0da245080e342c18f243d9a450e1ccb2bd12c51f
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-05-12 10:35:30 +0430

    Update sinchi.py

--- sinchi.py
+++ sinchi.py
@@ -5,8 +5,24 @@ import os
 redis = redis.Redis('localhost')
 id = input("Enter SinChi Number (1,2,3,4,5,...) : ")
 source = os.popen("cat ./bot.lua").read()
-launcher = """while true; do
+launcher = """#!/bin/bash
+while true; do
+sudo service tor start
+sudo service openvpn start
+http_proxy="socks5://127.0.0.1:9151"
+https_proxy="socks5://127.0.0.1:9151"
+ftp_proxy="socks5://127.0.0.1:9151"
+socks4_proxy="socks5://127.0.0.1:9151"
+socks5_proxy="socks5://127.0.0.1:9151"
+URL_proxy="socks5://127.0.0.1:9151"
+json_proxy="socks5://127.0.0.1:9151"
+redis-server_proxy="socks5://127.0.0.1:9151"
+redis_proxy="socks5://127.0.0.1:9151"
+ssl_proxy="socks5://127.0.0.1:9151"
+serpent_proxy="socks5://127.0.0.1:9151"
+while true; do
   ./telegram-cli -p sinchi-{} -s bot.lua
+sleep 3
 done""".format(id,id)
 newlauncher = open("sinchi-{}.sh".format(id),"w")
 newlauncher.write(launcher)
