405c2a348c861d11cb82976c2eabfaff77272be1
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-05-24 19:40:14 -0700

    Update sinchi.py

--- sinchi.py
+++ sinchi.py
@@ -7,17 +7,6 @@ id = input("Enter SinChi Number (1,2,3,4,5,...) : ")
 source = os.popen("cat ./bot.lua").read()
 launcher = """while true; do
  ./telegram-cli -p sinchi-{} -s bot.lua
-http_proxy="socks5://127.0.0.1:9151"
-https_proxy="socks5://127.0.0.1:9151"
-ftp_proxy="socks5://127.0.0.1:9151"
-socks4_proxy="socks5://127.0.0.1:9151"
-socks5_proxy="socks5://127.0.0.1:9151"
-URL_proxy="socks5://127.0.0.1:9151"
-json_proxy="socks5://127.0.0.1:9151"
-redis-server_proxy="socks5://127.0.0.1:9151"
-redis_proxy="socks5://127.0.0.1:9151"
-ssl_proxy="socks5://127.0.0.1:9151"
-serpent_proxy="socks5://127.0.0.1:9151"
 done""".format(id,id)
 newlauncher = open("sinchi-{}.sh".format(id),"w")
 newlauncher.write(launcher)
